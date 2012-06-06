a0a38bf1d45a3f5ca5917a46b40c538cf6d935f8
Chris <chris@pdthx.net>
2012-06-06 10:11:16 -0400

    blablah

---
+++ PdThx/MECodeSetupViewController.h
@@ -0,0 +1,17 @@
+//
+//  MECodeSetupViewController.h
+//  PdThx
+//
+//  Created by James Rhodes on 6/6/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "Environment.h"
+
+
+@interface MECodeSetupViewController : UIViewController
+
+
+
+@end
