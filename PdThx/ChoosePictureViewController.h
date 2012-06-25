80f0b4407e88d829f09e8d7d9f013041f69aa1dc
James Rhodes <jrhodes2705@gmail.com>
2012-06-25 11:32:30 -0400

    add image picker for personalization

---
+++ PdThx/ChoosePictureViewController.h
@@ -0,0 +1,29 @@
+//
+//  ChoosePictureViewController.h
+//  PdThx
+//
+//  Created by James Rhodes on 6/25/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "UIModalBaseViewController.h"
+#import "ASIHTTPRequest.h"
+#import "ASIFormDataRequest.h"
+#import "JSON.h"
+#import "Environment.h"
+
+@interface ChoosePictureViewController : UIModalBaseViewController<UIImagePickerControllerDelegate, UINavigationControllerDelegate> {
+	UIImageView * imageView;
+	UIButton * choosePhotoBtn;
+	UIButton * takePhotoBtn;
+    NSDictionary* images;
+}
+
+@property (nonatomic, retain) IBOutlet UIImageView * imageView;
+@property (nonatomic, retain) IBOutlet UIButton * choosePhotoBtn;
+@property (nonatomic, retain) IBOutlet UIButton * takePhotoBtn;
+
+-(IBAction) getPhoto:(id) sender;
+
+@end
