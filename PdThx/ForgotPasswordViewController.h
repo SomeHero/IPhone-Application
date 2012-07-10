20d8ba2514a1393b0bf324b998f5fd8219a47bf0
Edward Mitchell <edeesis@Edwards-Mac-Pro.local>
2012-07-10 12:45:11 -0400

    Adding Forgot Passcode

---
+++ PdThx/ForgotPasswordViewController.h
@@ -0,0 +1,13 @@
+//
+//  ForgotPassword.h
+//  PdThx
+//
+//  Created by Edward Mitchell on 7/10/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ForgotPassword : NSObject
+
+@end
---
+++ PdThx/ForgotPasswordViewController.m
@@ -0,0 +1,13 @@
+//
+//  ForgotPassword.m
+//  PdThx
+//
+//  Created by Edward Mitchell on 7/10/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "ForgotPassword.h"
+
+@implementation ForgotPassword
+
+@end
