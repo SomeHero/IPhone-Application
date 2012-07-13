8b51f00e4c9de55f76eda43cf1c0ab14fe22bf63
James Rhodes <jrhodes2705@gmail.com>
2012-07-13 10:27:46 -0400

    worked on do good screens

---
+++ PdThx/DetailInfoButtonClicked.h
@@ -0,0 +1,13 @@
+//
+//  DetailInfoButtonClicked.h
+//  PdThx
+//
+//  Created by James Rhodes on 7/12/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol DetailInfoButtonClicked <NSObject>
+
+@end
---
+++ PdThx/OrganizationDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  OrganizationDetailViewController.h
+//  PdThx
+//
+//  Created by James Rhodes on 7/12/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OrganizationDetailViewController : UIViewController
+
+@end
