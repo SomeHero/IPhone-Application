8fa3b8e48bf172e1b6e0122af57a7272274245ee
Chris <chris@pdthx.net>
2012-07-23 14:26:50 -0400

    Facebook Cancel Fix

---
+++ PdThx/FBHelperReturnProtocol.h
@@ -0,0 +1,15 @@
+//
+//  FBHelperReturnProtocol.h
+//  PdThx
+//
+//  Created by James Rhodes on 7/23/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol FBHelperReturnProtocol <NSObject>
+
+-(void)fbSignInCancelled;
+
+@end
--- PdThx/FacebookSignIn.h
+++ PdThx/FacebookSignIn.h
@@ -10,16 +10,18 @@
 #import "SignInWithFBService.h"
 #import "Facebook.h"
 #import "PdThxAppDelegate.h"
+#import "FBHelperReturnProtocol.h"
 
 
 @interface FacebookSignIn : NSObject<FBSessionDelegate, FBRequestDelegate> {
     SignInWithFBService *service;
     Facebook *fBook;
+    id<FBHelperReturnProtocol> cancelledDelegate;
     id userInfoDelegate;
 }
 
-
-
 - (void)signInWithFacebook:(id)sender;
+
+@property (assign) id cancelledDelegate;
 
 @end
