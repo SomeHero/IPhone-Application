275c06a91977407b910cc896efd280dce0b8469f
Chris Magee <chris@paidthx.com>
2012-08-29 13:12:18 -0400

    Implemented a more user-friendly paystream refresh

---
+++ PdThx/UIPaystreamLoadingCell.m
@@ -0,0 +1,29 @@
+//
+//  UIPaystreamLoadingCell.m
+//  PdThx
+//
+//  Created by Christopher Magee on 8/29/12.
+//
+//
+
+#import "UIPaystreamLoadingCell.h"
+
+@implementation UIPaystreamLoadingCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
